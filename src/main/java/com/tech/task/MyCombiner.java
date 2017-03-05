555a8f5b543a1f63e4ee3947004dd2c6260f38b3
Ivan Sobolev <ivan.sobolev@opower.com>
2017-03-05 14:11:59 +0200

    Elements now go to output queue

--- src/main/java/com/tech/task/MyCombiner.java
+++ src/main/java/com/tech/task/MyCombiner.java
@@ -15,6 +15,7 @@ public class MyCombiner<T> extends Combiner<T> {
     private final PriorityQueue<QueueMeta<T>> heap = new PriorityQueue<>();
     private final Map<BlockingQueue<T>, QueueMeta<T>> queues = new HashMap<>();
     private final CombinerWorker<T> worker;
+
     protected MyCombiner(SynchronousQueue<T> outputQueue) {
         super(outputQueue);
         this.worker = new CombinerWorker<T>(this);
@@ -24,6 +25,14 @@ public class MyCombiner<T> extends Combiner<T> {
     @Override
     public void addInputQueue(BlockingQueue<T> queue, double priority, long isEmptyTimeout, TimeUnit timeUnit)
             throws com.tech.task.Combiner.CombinerException {
+        if (priority <= 0) {
+            throw new IllegalArgumentException("Priority should be greater than zero. Was " + priority);
+        }
+        if (isEmptyTimeout <= 0) {
+            throw new IllegalArgumentException(
+                    "Timeout should be greater than zero. Was " + isEmptyTimeout + " " + timeUnit);
+        }
+
         QueueMeta<T> meta = new QueueMeta<T>(queue, priority, isEmptyTimeout, timeUnit);
         // TODO: this is not too safe, add a concurrency test
         this.heap.add(meta);
@@ -34,7 +43,7 @@ public class MyCombiner<T> extends Combiner<T> {
     public boolean hasInputQueue(BlockingQueue<T> queue) {
         return this.queues.containsKey(queue);
     }
-    
+
     @Override
     public void removeInputQueue(BlockingQueue<T> queue) {
         QueueMeta<T> meta = queues.remove(queue);
@@ -47,13 +56,15 @@ public class MyCombiner<T> extends Combiner<T> {
         this.worker.join();
     }
     // TODO: how do we clean up?
-    // TODO: how do we deal with race condition where an input queue was not empty when we've checked for an element and then it became 
+    // TODO: how do we deal with race condition where an input queue was not
+    // empty when we've checked for an element and then it became
     // TODO: thread to have daemon = true
-    
+
     private static class CombinerWorker<T> extends Thread {
         private static final AtomicLong instanceCounter = new AtomicLong();
         private MyCombiner<T> combiner;
         private AtomicBoolean continueFlag = new AtomicBoolean(true);
+
         CombinerWorker(MyCombiner<T> combiner) {
             setName("CombinerThread" + instanceCounter.incrementAndGet());
             setDaemon(true);
@@ -63,21 +74,41 @@ public class MyCombiner<T> extends Combiner<T> {
         public void shutdown() {
             this.continueFlag.set(false);
         }
-        
+
         @Override
         public void run() {
             while (continueFlag.get()) {
-                // TODO: rather than spin checking everything, derive next event of interest and sleep until then
+                // TODO: rather than spin checking everything, derive next event
+                // of interest and sleep until then
                 Collection<QueueMeta<T>> copy = new ArrayList<QueueMeta<T>>(combiner.heap);
                 for (QueueMeta<T> meta : copy) {
                     if (meta.isExpiredAndEmpty()) {
                         combiner.removeInputQueue(meta.queue);
                     }
+
+                    T result = meta.tryRead();
+                    // TODO: what do we do if a queue was removed while we were
+                    // waiting for the consumer to accept the thing?
+                    // TODO: how we remove queues which were timed out while we
+                    // were waiting for the consumer to get the value?
+                    // having a single thread managing all the meta is kinda fun
+                    // in terms of requiring little-to-no sync
+                    // having two threads allows to spread the responsibilities
+                    if (result == null) {
+                        continue;
+                    }
+
+                    try {
+                        this.combiner.outputQueue.put(result);
+                    } catch (InterruptedException ex) {
+                        // TODO: log instead
+                        ex.printStackTrace();
+                    }
                 }
             }
         }
     }
-    
+
     private static class QueueMeta<T> {
         final long addedAtNano;
         long expirationNano;
@@ -85,6 +116,7 @@ public class MyCombiner<T> extends Combiner<T> {
         final double priority;
         final long isEmptyTimeout;
         final TimeUnit timeUnit;
+
         QueueMeta(BlockingQueue<T> queue, double priority, long isEmptyTimeout, TimeUnit timeUnit) {
             super();
             this.queue = queue;
@@ -92,7 +124,25 @@ public class MyCombiner<T> extends Combiner<T> {
             this.isEmptyTimeout = isEmptyTimeout;
             this.timeUnit = timeUnit;
             this.addedAtNano = System.nanoTime();
-            this.expirationNano = timeUnit.toNanos(isEmptyTimeout) + addedAtNano;
+            refreshExpiration();
+        }
+
+        private T tryRead() {
+            try {
+                T element = this.queue.poll(5, TimeUnit.MILLISECONDS);
+                if (element != null) {
+                    this.refreshExpiration();
+                }
+                return element;
+            } catch (InterruptedException ex) {
+                // TODO: use logging instead to have a thread name captured etc
+                ex.printStackTrace();
+                return null;
+            }
+        }
+
+        private void refreshExpiration() {
+            this.expirationNano = this.timeUnit.toNanos(this.isEmptyTimeout) + this.addedAtNano;
         }
 
         boolean isExpiredAndEmpty() {
--- src/test/java/com/tech/task/CombinerTest.java
+++ src/test/java/com/tech/task/CombinerTest.java
@@ -1,5 +1,8 @@
 package com.tech.task;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.SynchronousQueue;
@@ -63,5 +66,38 @@ public class CombinerTest {
         TimeUnit.MILLISECONDS.sleep(SLEEP_BUFFER + timeout);
 
         Assert.assertTrue(combiner.hasInputQueue(queue1));
+    }
+
+    @Test
+    public void testElementsFlowToOutputQueue() throws Exception {
+        SynchronousQueue<String> outputQueue = new SynchronousQueue<String>();
+        BlockingQueue<String> queue1 = new LinkedBlockingQueue<String>();
+        MyCombiner<String> combiner = new MyCombiner<String>(outputQueue);
+        combiner.addInputQueue(queue1, 1.0, 1, TimeUnit.HOURS);
+        queue1.add("element");
+        String result = outputQueue.poll(5, TimeUnit.MILLISECONDS);
+        Assert.assertEquals("element", result);
+    }
+
+    @Test
+    public void testPrioritiesAreRespected() throws Exception {
+        SynchronousQueue<String> outputQueue = new SynchronousQueue<String>();
+        BlockingQueue<String> queue1 = new LinkedBlockingQueue<String>();
+        BlockingQueue<String> queue2 = new LinkedBlockingQueue<String>();
+        MyCombiner<String> combiner = new MyCombiner<String>(outputQueue);
+        combiner.addInputQueue(queue1, 1.0, 1, TimeUnit.HOURS);
+        combiner.addInputQueue(queue2, 2.0, 1, TimeUnit.HOURS);
+        queue1.add("element1");
+        queue1.add("element1");
+        queue2.add("element2");
+        queue2.add("element2");
+
+        List<String> elements = new ArrayList<>();
+        int number = outputQueue.drainTo(elements, 3);
+        Assert.assertEquals(3, number);
+        int elementCount1 = Collections.frequency(elements, "element1");
+        int elementCount2 = Collections.frequency(elements, "element2");
+        Assert.assertEquals(1, elementCount1);
+        Assert.assertEquals(2, elementCount2);
     }
 }
